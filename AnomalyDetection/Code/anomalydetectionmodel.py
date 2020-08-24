95b9a97e5849c3954cb7ac0b2cff37d9fd4b25d7
bberry <bberry@in.imshealth.com>
2020-08-25 00:12:41 +0530

    Updating some comments

--- AnomalyDetection/Code/anomalydetectionmodel.py
+++ AnomalyDetection/Code/anomalydetectionmodel.py
@@ -1,27 +1,27 @@
-# -*- coding: utf-8 -*-
-
+#-------------------------------------------
+#import Libraries
 import time
 import math
 import scipy.io as sio
 import numpy as np
 import matplotlib.pyplot as plt
-
-tic = time.time
-
 import os
+#-------------------------------------------
+# Read the data into data
+tic = time.time
 path  = os.path.abspath("AnomalyDetection/Data/anomalyData.mat")
-
 data = sio.loadmat(path)
 
 X = data['X']
 X_val = data['Xval']
 y_val = data['yval']
+
+#--------------------------------------------
 y_val
 
 plt.scatter(X[:,0], X[:,1], marker='X')
 plt.xlabel('Latency(ms)')
 plt.ylabel("Throughput mb/s ")
-
 
 #-----------------------------------
 def estimateGaussian(X):
@@ -55,25 +55,19 @@ def multivariateGaussian(X, mu, sigma2):
      val = np.reshape((-0.5)*np.sum(np.multiply((X@inv),X), 1),(np.size(X, 0), 1))
      
      p = np.power(2*pi, -n/2)*np.power(det, -0.5)*np.exp(val)
-     
      return p
- 
 
 p = multivariateGaussian(X, mu, sigma2)
-
 print(p.shape)
-
 pval = multivariateGaussian(X_val, mu, sigma2)
 
 #-----------------------------------
 def selectThreshHold(yval, pval):
-    
     F1 = 0
     bestF1 = 0
     bestEpsilon = 0
     
     stepsize = (np.max(pval) - np.min(pval))/1000
-        
     epsVec = np.arange(np.min(pval), np.max(pval), stepsize)
     noe = len(epsVec)
     
@@ -104,9 +98,8 @@ def selectThreshHold(yval, pval):
 
 F1, epsilon = selectThreshHold(y_val, pval)
 print('Epsilon and F1 are:',epsilon, F1)
-
 outl = (p < epsilon)
-
+#-----------------------------------
 def findIndices(binVec):
     l = []
     for i in range(len(binVec)):
@@ -119,9 +112,9 @@ count_outliers = len(listOfOutliers)
 print('\n\nNumber of outliers:', count_outliers)
 print('\n',listOfOutliers)
 
-
 plt.scatter(X[:, 0], X[:, 1], marker = "x")
 plt.xlabel('Latency(ms)')
 plt.ylabel('Throughput(mb/s)')
 plt.scatter(X[listOfOutliers,0], X[listOfOutliers, 1], facecolors = 'none', edgecolors = 'r')
 plt.show()
+#-----------------------------------
--- AnomalyDetection/Code/detect_fix_outlier.py
+++ AnomalyDetection/Code/detect_fix_outlier.py
@@ -1,15 +1,18 @@
-# -*- coding: utf-8 -*-
 """
 Detecting outliers and fixing them
 > Boston House Pricing Datase
 """
-
+#---------------------------------------------
 #Import the libraries
 import pandas as pd
 from sklearn.datasets import load_boston
-
 import matplotlib.pyplot as plt
 
+import seaborn as sns
+from scipy import stats
+import numpy as np
+#---------------------------------------------
+# Read the dataset
 boston = load_boston()
 x = boston.data
 y = boston.target
@@ -21,9 +24,8 @@ boston_df_o1= boston_df[:]
 boston_df_o= boston_df[:]
 boston_df.columns = columns
 boston_df.head()
-
+#---------------------------------------------------
 #1.Discovering the outliers with visualization tools
-import seaborn as sns
 #Box plot
 sns.boxplot(x=boston_df['DIS'])
 
@@ -35,24 +37,20 @@ ax.set_ylabel('Full-value property-tax rate per $10,000')
 plt.show()
 
 #2. Discover the outlier using the mathamatical function 
-from scipy import stats
-import numpy as np
-# using Z-score
+# Using Z-score
 z = np.abs(stats.zscore(boston_df))
 print(z)
 
 threshold = 3
 print(np.where(z > 3))
 
-#using IQR
+#Using IQR
 Q1 = boston_df_o1.quantile(0.25)
 Q3 = boston_df_o1.quantile(0.75)
 IQR = Q3 - Q1
 print(IQR)
-
 print((boston_df_o1 < (Q1 - 1.5 * IQR)) | (boston_df_o1 > (Q3 + 1.5 * IQR)))
-
-
+#---------------------------------------------------
 #Correcting the Outlier: COrrecting or removing
 #1. using Z-score
 boston_df_o = boston_df_o[(z < 3).all(axis=1)]
